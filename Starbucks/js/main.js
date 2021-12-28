5e70ca7d4f9685862933dbb677d0e49d4324627a
BSH <bsh93367@gmail.com>
2021-12-28 17:02:47 +0900

    visual section 완료

--- Starbucks/js/main.js
+++ Starbucks/js/main.js
@@ -1,35 +1,42 @@
-const searchEl=document.querySelector('.search');
-const searchInputEl=searchEl.querySelector('input');
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click',function(){
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
 
-searchInputEl.addEventListener('focus',function(){
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
-searchInputEl.addEventListener('blur',function(){
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
+  searchInputEl.setAttribute('placeholder', '');
 });
 
-const badgeEl=document.querySelector('header .badges');
-//_.throttle(함수,시간ms)
-window.addEventListener('scroll', _.throttle(function(){
+const badgeEl = document.querySelector('header .badges');
+//_.throttle(함수,시간(ms))
+window.addEventListener('scroll', _.throttle(function () {
   console.log(window.scrollY);
-  if(window.scrollY>500){
-    //gsap.toString(요소, 지속시간s, 옵션);
-    gsap.to(badgeEl,.6,{
-      opacity:0,
-      display:'none'
+  if (window.scrollY > 500) {
+    //gsap.toString(요소, 지속시간(s), 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
     });
-  }
-  else{
-    gsap.to(badgeEl,.6,{
-      opacity:1,
-      display:'block'
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
     });
   }
 }, 300));
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, //0.7, 1.4, 2.1, 2.8
+    opacity: 1
+  });
+});
