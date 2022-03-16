e8ccf42d0993b27be4966f44bd7107c8fb85c704
SeungHak5161 <bsh93367@gmail.com>
2022-03-16 23:49:01 +0900

    React study 22.03.16 state

--- React-Study/test/my-app/src/App.js
+++ React-Study/test/my-app/src/App.js
@@ -1,10 +1,12 @@
 import Composition from './component/Composition';
 import Extraction from './component/Extraction/Extraction';
 import './App.css';
+import ClassComponent from './component/state/ClassComponent';
 
 function App() {
   return (
     <div className="App">
+     <ClassComponent/>
      <Composition />
      <Extraction/>
     </div>
