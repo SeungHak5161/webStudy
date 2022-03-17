079f2256bb6f92eb4ecdd4789640714a94f470cf
SeungHak5161 <bsh93367@gmail.com>
2022-03-17 18:56:32 +0900

    React study 22.03.17 state to functional

--- React-Study/test/my-app/src/App.js
+++ React-Study/test/my-app/src/App.js
@@ -2,10 +2,12 @@ import Composition from './component/Composition';
 import Extraction from './component/Extraction/Extraction';
 import './App.css';
 import ClassComponent from './component/state/ClassComponent';
+import FunctionalComponent from './component/state/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+     <FunctionalComponent/>
      <ClassComponent/>
      <Composition />
      <Extraction/>
--- React-Study/test/my-app/src/component/state/FunctionalComponent.jsx
+++ React-Study/test/my-app/src/component/state/FunctionalComponent.jsx
@@ -1,11 +1,20 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function FunctionalComponent() {
   const [date,setDate]=useState(new Date())
+
+  const tick = () => {
+    setDate(new Date())
+  }
+  
+  useEffect(()=>{
+    const interval = setInterval(()=>tick(), 1000)
+  }, [])
+
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      <h1>Hi! This is Functional</h1>
+      <h2>It is {date.toLocaleTimeString()}.</h2>
     </div>
   )
 }
