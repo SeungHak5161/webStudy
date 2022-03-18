b55434b52d7045006dd5b2d73086590ef9a66aaa
BSH <bsh93367@gmail.com>
2022-03-18 16:16:01 +0900

    React study 22.03.18 event

--- React-Study/test/my-app/src/App.js
+++ React-Study/test/my-app/src/App.js
@@ -1,15 +1,17 @@
+import './App.css';
 import Composition from './component/Composition';
 import Extraction from './component/Extraction/Extraction';
-import './App.css';
 import ClassComponent from './component/State/ClassComponent';
 import FunctionalComponent from './component/State/FunctionalComponent';
 import ClassComponent2 from './component/LifeCycle/ClassComponent';
+import Event from './component/Event/Event';
 
 function App() {
   return (
     <div className="App">
-      <ClassComponent2/>
-      {/* <FunctionalComponent/>
+      <Event />
+      {/* <ClassComponent2/>
+      <FunctionalComponent/>
       <ClassComponent/>
       <Composition />
       <Extraction/> */}
---
+++ React-Study/test/my-app/src/component/Event/Event.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+// capturing은 자식 컴포넌트의 호출을 확인함(자식보다 부모가 먼저 인지)
+// bubbling은 captureing 이후 호출된 event가 완료되고 호출됨
+export default function Event() {
+  const handleButtonClick=(e)=>{
+    console.dir(e)
+    console.log('handleButtonClick')
+  }
+  const handleClickCapture=()=>{
+    console.log('handleClickCapture')
+  }
+  const handleClickCapture2=()=>{
+    console.log('handleClickCapture2')
+  }
+  const handleClickBubble=()=>{
+    console.log('handleClickBubble')
+  }
+  const handleMouseLeave=(e)=>{
+    console.dir(e)
+    console.log('handleMouseLeave')
+  }
+  return (
+    <div onClickCapture={handleClickCapture}>
+      <div onClickCapture={handleClickCapture2} onClick={handleClickBubble}>
+        <button onClick={handleButtonClick} onMouseLeave={handleMouseLeave}>Button</button>
+      </div>
+    </div>
+    )
+}
