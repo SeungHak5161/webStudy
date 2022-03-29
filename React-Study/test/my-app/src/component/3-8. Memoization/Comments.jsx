6e324d7c5512acf6e6ab60e7a4152a0f1d4971bd
SeungHak5161 <bsh93367@gmail.com>
2022-03-29 16:58:28 +0900

    React study 22.03.29 memorization, profiler, useCallback

--- React-Study/test/my-app/src/component/3-8. Memoization/Comments.jsx
+++ React-Study/test/my-app/src/component/3-8. Memoization/Comments.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useCallback} from 'react'
 import CommentItem from './CommentItem'
 
 export default function Comments(props) {
+  // 전달 받는 props가 계속해서 변경되기 때문에 handleChange 함수도 계속해서 변경됨
+  // 이를 방지하기 위해서 useCallback() 사용 <- 이것 또한 memorization
+  const handleChange = useCallback(()=>{
+    console.log("Click")
+  },[])
   return (
     <div>
       {props.commentList.map(comment=><CommentItem
@@ -9,6 +14,7 @@ export default function Comments(props) {
         // title={comment.title}
         // content={comment.content}
         // likes={comment.likes}
+        onClick={handleChange}
         {...comment}
       />)}
     </div>
--- React-Study/test/my-app/src/component/3-8. Memoization/Memo.jsx
+++ React-Study/test/my-app/src/component/3-8. Memoization/Memo.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import Comments from "./Comments"
 
 const commentList=[
-  {title:"comment1", content:"message1",likes:10},
-  {title:"comment2", content:"message2",likes:20},
-  {title:"comment3", content:"message3",likes:30}
+  {title:"comment1", content:"message1",likes:1},
+  {title:"comment2", content:"message2",likes:2},
+  {title:"comment3", content:"message3",likes:3}
 ]
 export default function Memo() {
+  // 아래 useEffect로 comments가 계속 바뀌었는데, 
+  // CommentItem.jsx에서 memo(rization)를 사용함으로써 변경 부분만 랜더링되게 바뀜
   const [comments, setComments]=useState(commentList)
 
   useEffect(() => {
@@ -16,7 +18,7 @@ export default function Memo() {
         {
         title:`comment${prevComments.length+1}`,
         content:`message${prevComments.length+1}`,
-        likes:`${prevComments.length+10}`
+        likes:`${prevComments.length+1}`
         }
       ])
     },1000)
