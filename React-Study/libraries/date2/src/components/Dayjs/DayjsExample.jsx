30c515cfa685119827b6298ec5d28f5e7d3a1f0c
SeungHak5161 <bsh93367@gmail.com>
2022-04-19 17:38:20 +0900

    React study 22.04.19 library-day.js

--- React-Study/libraries/date2/src/components/Dayjs/DayjsExample.jsx
+++ React-Study/libraries/date2/src/components/Dayjs/DayjsExample.jsx
@@ -1,12 +1,18 @@
 import React, { useRef, useState } from "react";
-import moment from "moment-timezone";
-import "moment/locale/ko";
+import dayjs from "dayjs";
+import "dayjs/locale/ko";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
 
-export default function MomentExample() {
+dayjs.locale("ko");
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+export default function DayjsExample() {
   const birthDayRef = useRef(null);
   const [day, setDay] = useState("");
   const handleBirthDayChange = (e) => {
-    setDay(moment(e.target.value, "YYYY-MM-DD").format("dddd"));
+    setDay(dayjs(e.target.value, "YYYY-MM-DD").format("dddd"));
   };
 
   const formatToPositive = (props) => {
@@ -16,33 +22,35 @@ export default function MomentExample() {
       return props;
     }
   };
-  const momentDate = moment();
-  const newMomentDate = momentDate.add(1, "week");
-  const cloneNewMomentDate = newMomentDate.clone().add(1, "week");
+  const dayjsDate = dayjs();
+  const newDayjsDate = dayjsDate.add(1, "week");
+  const cloneNewDayjsDate = newDayjsDate.clone().add(1, "week");
   return (
     <div>
-      <h1>Moment</h1>
+      <h1>Day.js</h1>
       <div>Immutable Check</div>
       <div>
-        {momentDate.format()}
+        {dayjsDate.format()}
         <br />
-        {newMomentDate.format()}
+        {newDayjsDate.format()}
         <br />
-        {cloneNewMomentDate.format()}
+        {cloneNewDayjsDate.format()}
         <br />
       </div>
       <br />
+      {/* moment와 차이점 : 24시간을 더했을 경우 알아서 계산하고 1일 더한것과 같이 출력 */}
       <div>Summer Time (NewYork)</div>
+      <div>{dayjs.tz.guess()}</div>
       <div>
         2018/3/10 13시에 하루 더하기 :
-        {moment
+        {dayjs
           .tz("2018-03-10 13:00:00", "America/New_York")
           .add(1, "day")
           .format()}
       </div>
       <div>
         2018/3/10 13시에 24시간 더하기 :
-        {moment
+        {dayjs
           .tz("2018-03-10 13:00:00", "America/New_York")
           .add(24, "hour")
           .format()}
@@ -51,15 +59,15 @@ export default function MomentExample() {
       <div>Leap year (Korea)</div>
       <div>
         2017//1/1에 1년 빼기 :
-        {moment("2017-01-01 00:00:00").subtract(1, "year").format()}
+        {dayjs("2017-01-01 00:00:00").subtract(1, "year").format()}
       </div>
       <div>
         2017//1/1에 365일 빼기 :
-        {moment("2017-01-01 00:00:00").subtract(365, "day").format()}
+        {dayjs("2017-01-01 00:00:00").subtract(365, "day").format()}
       </div>
       <br />
       <div>한국어로 표기</div>
-      <div>{moment("2017-07-17 00:00:00").format("YYYY년 M월 D일")}</div>
+      <div>{dayjs("2017-07-17 00:00:00").format("YYYY년 M월 D일")}</div>
       <br />
       <div>생일 요일 찾기</div>
       <div>
@@ -68,7 +76,7 @@ export default function MomentExample() {
       </div>
       <div>두 날짜 비교</div>
       <div>{`${formatToPositive(
-        moment("2022-04-18").diff(moment("2022-09-23"), "day")
+        dayjs("2022-04-18").diff(dayjs("2022-09-23"), "day")
       )}일 차이`}</div>
     </div>
   );
