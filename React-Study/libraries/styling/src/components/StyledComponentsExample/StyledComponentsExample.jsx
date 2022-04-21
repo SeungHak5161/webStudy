bcf464c6f5c6b234d63d4fb736327048cf7f314f
SeungHak5161 <bsh93367@gmail.com>
2022-04-21 15:26:24 +0900

    React study 20.04.21 libray-styledComponent

--- React-Study/libraries/styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
+++ React-Study/libraries/styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -1,5 +1,61 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function StyledComponentsExample() {
-  return <div>StyledComponentsExample</div>;
+  // Create a Title component that'll render an <h1> tag with some styles
+  const Title = styled.h1`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+  `;
+
+  // Create a Wrapper component that'll render a <section> tag with some styles
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `;
+
+  const Button = styled.button`
+    /* Adapt the colors based on primary prop */
+    background: ${(props) => (props.primary ? "palevioletred" : "white")};
+    color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid palevioletred;
+    border-radius: 3px;
+    cursor: pointer;
+  `;
+
+  // styled() : 상속
+  const TomatoButton = styled(Button)`
+    color: tomato;
+    border-color: tomato;
+  `;
+
+  const ReversedButton = (props) => (
+    <Button {...props} children={props.children.split("").reverse()} />
+  );
+
+  return (
+    <>
+      <Wrapper>
+        <Title>Hello World!</Title>
+      </Wrapper>
+      <Button onClick={() => alert("normal")}>Normal</Button>
+      <Button primary onClick={() => alert("primary")}>
+        Primary
+      </Button>
+      <TomatoButton>Tomato</TomatoButton>
+      <br />
+      <Button as="a" href="javascript:void(0)">
+        Button as "a"
+      </Button>
+      <br />
+      <Button as={ReversedButton}>
+        Custom Button with Normal Button styles
+      </Button>
+    </>
+  );
 }
