b0ed856a9886965e45c9f4295467b078ec00a2f3
SeungHak5161 <bsh93367@gmail.com>
2022-04-21 20:30:07 +0900

    React study 20.04.21 libray-styledComponent themming, globalStyling

--- React-Study/libraries/styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
+++ React-Study/libraries/styling/src/components/StyledComponentsExample/StyledComponentsExample.jsx
@@ -1,139 +1,62 @@
-import React from "react";
-import styled, { keyframes } from "styled-components";
+import React, { useState } from "react";
+import styled, { createGlobalStyle, ThemeProvider } from "styled-components";
 
-// const Title = styled.h1`
-//   font-size: 1.5em;
-//   text-align: center;
-//   color: palevioletred;
-// `;
+const Button = styled.button`
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border-radius: 3px;
 
-// const Wrapper = styled.section`
-//   padding: 4em;
-//   background: papayawhip;
-// `;
-
-// const Button = styled.button`
-//   /* Adapt the colors based on primary prop */
-//   background: ${(props) => (props.primary ? "palevioletred" : "white")};
-//   color: ${(props) => (props.primary ? "white" : "palevioletred")};
-
-//   font-size: 1em;
-//   margin: 1em;
-//   padding: 0.25em 1em;
-//   border: 2px solid palevioletred;
-//   border-radius: 3px;
-//   cursor: pointer;
-// `;
-
-// // styled() : 상속
-// const TomatoButton = styled(Button)`
-//   color: tomato;
-//   border-color: tomato;
-// `;
-
-// const ReversedButton = (props) => (
-//   <Button {...props} children={props.children.split("").reverse()} />
-// );
-
-// // css 선택자 예시
-// const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
-//   color: blue;
-
-//   &:hover {
-//     color: red; // <Thing> when hovered
-//   }
-
-//   & ~ & {
-//     background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
-//   }
-
-//   & + & {
-//     background: lime; // <Thing> next to <Thing>
-//   }
-
-//   &.something {
-//     background: orange; // <Thing> tagged with an additional CSS class ".something"
-//   }
-
-//   .something-else & {
-//     border: 1px solid; // <Thing> inside another element labeled ".something-else"
-//   }
-// `;
-
-// const Input = styled.input.attrs((props) => ({
-//   type: "text",
-//   size: props.size || "1em",
-// }))`
-//   border: 2px solid palevioletred;
-//   margin: ${(props) => props.size};
-//   padding: ${(props) => props.size};
-// `;
-
-// // .attrs() : props를 override 할 수 있음
-// const PasswordInput = styled(Input).attrs({
-//   type: "password",
-// })`
-//   // similarly, border will override Input's border
-//   border: 2px solid aqua;
-// `;
-
-const rotate = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
+  /* Color the border and text with theme.main */
+  color: ${(props) => props.theme.main};
+  border: 2px solid ${(props) => props.theme.main};
 `;
 
-// Here we create a component that will rotate everything we pass in over two seconds
-const Rotate = styled.div`
-  display: inline-block;
-  animation: ${rotate} 2s linear infinite;
-  padding: 2rem 1rem;
-  font-size: 1.2rem;
+// .defaultProps : 말 그대로 기본값 지정가능
+Button.defaultProps = {
+  theme: { main: "palevioletred" },
+};
+
+const defaultTheme = {
+  main: "palevioletred",
+};
+
+const greenTheme = {
+  main: "mediumseagreen",
+};
+
+// createGlobalStyle 전역에 적용해야 할 속성 지정, html 전체에 적용됨
+const GlobalStyle = createGlobalStyle`
+  button{
+    background-color: pink;
+  }
 `;
 
 export default function StyledComponentsExample() {
+  const [theme, setTheme] = useState(greenTheme);
+
+  const handleTheme = (e) => {
+    setTheme(e);
+    console.log(theme);
+  };
+
   return (
     <>
-      <>
-        <Rotate>&lt; 💅🏾 &gt;</Rotate>
-      </>
-      {/* <>
-        <div>
-          <Input placeholder="A bigger text input" size="2em" />
-          <br />
-          <PasswordInput placeholder="A bigger password input" size="2em" />
-        </div>
-      </> */}
-      {/* <>
-        <Thing>Hello world!</Thing>
-        <Thing>How ya doing?</Thing>
-        <Thing className="something">The sun is shining...</Thing>
-        <div>Pretty nice day today.</div>
-        <Thing>Don't you think?</Thing>
-        <div className="something-else">
-          <Thing>Splendid.</Thing>
-        </div>
-      </> */}
-      {/* <Wrapper>
-        <Title>Hello World!</Title>
-      </Wrapper>
-      <Button onClick={() => alert("normal")}>Normal</Button>
-      <Button primary onClick={() => alert("primary")}>
-        Primary
-      </Button>
-      <TomatoButton>Tomato</TomatoButton>
-      <br />
-      <Button as="a" href="javascript:void(0)">
-        Button as "a"
-      </Button>
-      <br />
-      <Button as={ReversedButton}>
-        Custom Button with Normal Button styles
-      </Button> */}
+      <div>
+        <GlobalStyle />
+        <button onClick={() => handleTheme(defaultTheme)}>red</button>
+        <button onClick={() => handleTheme(greenTheme)}>green</button>
+
+        <Button>Normal</Button>
+
+        {/* <ThemeProvider/> : context.provider처럼 동작함 */}
+        <ThemeProvider theme={theme}>
+          <Button>Themed</Button>
+        </ThemeProvider>
+      </div>
+      <div>
+        <button>Is GlobalStyle influence outter area?</button>
+      </div>
     </>
   );
 }
