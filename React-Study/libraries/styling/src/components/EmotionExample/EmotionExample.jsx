063b85c1016d7443e5e4f8364027e5318bd86544
SeungHak5161 <bsh93367@gmail.com>
2022-04-25 00:50:00 +0900

    React study 22.04.25 library-emotion pass

--- React-Study/libraries/styling/src/components/EmotionExample/EmotionExample.jsx
+++ React-Study/libraries/styling/src/components/EmotionExample/EmotionExample.jsx
@@ -1,5 +1,9 @@
+// styling library 다 비슷하고 어렵지도 않아서 그냥 쓸때 레퍼런스 보면 될듯 pass
+
 import React from "react";
-import { css, cx } from "@emotion/css";
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+// styled-component와 동일한 방식으로 사용 가능
 import styled from "@emotion/styled";
 
 export default function EmotionExample() {
@@ -13,21 +17,50 @@ export default function EmotionExample() {
       color: white;
     }
   `;
+
   const color = "white";
 
-  return (
-    <div
-      css={css`
-        padding: 32px;
-        background-color: hotpink;
-        font-size: 24px;
-        border-radius: 4px;
-        &:hover {
-          color: ${color};
-        }
-      `}
-    >
-      Hover to change color.
+  const style = css`
+    color: hotpink;
+  `;
+
+  const SomeComponent = ({ children }) => (
+    <div css={style}>
+      Some hotpink text.
+      {children}
     </div>
+  );
+
+  const anotherStyle = css({
+    textDecoration: "underline",
+  });
+
+  const AnotherComponent = () => (
+    <div css={anotherStyle}>Some text with an underline.</div>
+  );
+
+  return (
+    <>
+      <div
+        css={css`
+          padding: 32px;
+          background-color: hotpink;
+          font-size: 24px;
+          border-radius: 4px;
+          &:hover {
+            color: ${color};
+          }
+        `}
+      >
+        Hover to change color.
+      </div>
+      <br />
+      <Button>This my button component.</Button>
+
+      <SomeComponent>
+        <AnotherComponent />
+      </SomeComponent>
+      <AnotherComponent />
+    </>
   );
 }
