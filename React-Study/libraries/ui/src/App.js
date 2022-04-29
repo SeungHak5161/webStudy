61f6f5031e0f817e3816a2cb748206ad84814429
BSH <bsh93367@gmail.com>
2022-04-29 17:04:03 +0900

    React study 22.04.29 Ant design fin

--- React-Study/libraries/ui/src/App.js
+++ React-Study/libraries/ui/src/App.js
@@ -10,15 +10,19 @@ import ActionSheetExample from "./components/OnsenUI/ActionSheetExample";
 import AntDesign from "./components/AntDesign/AntDesign";
 import IconExample from "./components/AntDesign/IconExample";
 import TypographyExample from "./components/AntDesign/TypographyExample";
+import LayoutExample from "./components/AntDesign/LayoutExample";
+import FormExample from "./components/AntDesign/FormExample";
 
-// 학습 하는 중에 원인 모를 에러가 계속해서 발생, 호환성 맞춰서 재시도 해 봤지만 소용없음
+// onsenui -> 학습 하는 중에 원인 모를 에러가 계속해서 발생, 호환성 맞춰서 재시도 해 봤지만 소용없음
 // React에 대한 지원이 빈약한 라이브러리라고 생각되어(그리고 하루종일 삽질하다 혈압올라서) pass
 function App() {
   return (
     <>
-      <TypographyExample />
+      <FormExample />
+      {/* <LayoutExample /> */}
+      {/* <TypographyExample />
       <IconExample />
-      <AntDesign />
+      <AntDesign /> */}
       {/* <Page>
         <OnsenUIExample />
         <ButtonExample />
---
+++ React-Study/libraries/ui/src/components/AntDesign/LayoutExample.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
+const items1 = ["1", "2", "3"].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
+
+export default function LayoutExample() {
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Header className="header">
+        <div
+          className="logo"
+          style={{
+            backgroundColor: "pink",
+            width: 30,
+            height: 30,
+            float: "left",
+          }}
+        />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={items1}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: "0 50px",
+        }}
+      >
+        <Breadcrumb
+          style={{
+            margin: "16px 0",
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <Layout
+          className="site-layout-background"
+          style={{
+            padding: "24px 0",
+            backgroundColor: "white",
+          }}
+        >
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["6"]}
+              defaultOpenKeys={["sub2"]}
+              style={{
+                height: "100%",
+              }}
+              items={items2}
+            />
+          </Sider>
+          <Content
+            style={{
+              padding: "0 24px",
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Content>
+      <Footer
+        style={{
+          textAlign: "center",
+        }}
+      >
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
+}
